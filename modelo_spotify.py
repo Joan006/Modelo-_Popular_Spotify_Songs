c7cba22d7d4040198361f983d6d49074f526ebf1
JoanMdv <joan_martinez.olivares@hotmail.com>
2024-05-31 00:37:42 -0600

    Se comeinza modelo - planeacioni.2

--- modelo_spotify.py
+++ modelo_spotify.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as nd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
 
@@ -18,8 +20,18 @@ df_spotify["streams"] = pd.to_numeric(df_spotify["streams"], errors = "coerce")
 df_spotify["in_deezer_playlists"] = pd.to_numeric(df_spotify["in_deezer_playlists"], errors="coerce")
 df_spotify["in_shazam_charts"] = pd.to_numeric(df_spotify["in_shazam_charts"], errors="coerce")
 
-# Comenzamos con la limpieza de datos
+# Comprobamos si hay missing values
 print(df_spotify.isnull().sum())
 
-for art in df_spotify["artist(s)_name"]:
-    print(art)
+# Eliminamos los missing values
+df_spotify.dropna(subset=['streams', 'in_deezer_playlists', 'in_shazam_charts', 'key'], inplace=True)
+
+variables_numerias =  ['streams', 'in_spotify_playlists', 'in_spotify_charts', 'in_apple_playlists', 'in_apple_charts','in_deezer_playlists', 'in_deezer_charts', 'in_shazam_charts', 'bpm', 'danceability_%', 'valence_%', 'energy_%', 'acousticness_%', 'instrumentalness_%', 'liveness_%', 'speechiness_%']
+
+plt.figure(figsize=(8, 10))
+for i, col in enumerate(variables_numerias, 1):
+    plt.subplot(5, 4, i)
+    sns.histplot(df_spotify[col], kde=True, bins=30)
+    plt.title(f'Distribution of {col}')
+plt.tight_layout()
+plt.show()
